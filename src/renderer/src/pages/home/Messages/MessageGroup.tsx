8f669ecf55b7c1de74b9f55012e1744608f82524
eastling <vanre.wu@gmail.com>
2025-07-13 21:49:45 +0800

    Enable pointer events for grid popover message

--- src/renderer/src/pages/home/Messages/MessageGroup.tsx
+++ src/renderer/src/pages/home/Messages/MessageGroup.tsx
@@ -358,7 +358,7 @@ const MessageWrapper = styled.div<MessageWrapperProps>`
     cursor: default;
     .message-content-container {
       padding-left: 0;
-      pointer-events: all;
+      pointer-events: auto;
     }
     .MessageFooter {
       margin-left: 0;
